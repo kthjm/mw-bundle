290fc7259ff1ec6cef9036dd92e9f27ae18ccaaa
kthjm <toxictoxer@gmail.com>
2018-05-10 05:18:07 +0900

    Add prepack-webpack-plugin(But not work in webpack 4)

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,8 +1,17 @@
 import webpack from 'webpack'
 import merge from 'webpack-merge'
+import PrepackWebpackPlugin from 'prepack-webpack-plugin'
 import { resolve } from 'path'
 
-export const baseConfig = {
+let manifest
+try { manifest = require('./dist/webpack/dev/dll.manifest.json') }
+catch(e) { console.log('manifest is not exist.') }
+
+const devdir = resolve(__dirname, 'dist/webpack/dev')
+const dllname = 'dll'
+const port = 7000
+const publicPath = `http://localhost:${port}/`
+const baseConfig = {
   entry: './src',
   module: {
     rules: [
@@ -11,13 +20,74 @@ export const baseConfig = {
   }
 }
 
-export default merge(baseConfig, {
-  mode: 'production',
-  output: {
-    filename: 'index.js',
-    path: resolve(__dirname, 'dist/webpack/pro')
+const configs = {
+
+  dll: {
+    mode: 'none',
+    entry: [
+      'react',
+      'react-dom',
+      'atra'
+    ],
+    output: {
+      filename: `${dllname}.js`,
+      path: devdir,
+      library: dllname
+    },
+    plugins: [
+      new webpack.DllPlugin({
+        path: resolve(devdir, `${dllname}.manifest.json`),
+        name: dllname
+      })
+    ]
   },
-  plugins: [
-    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
-  ]
-})
+
+  dev: merge(baseConfig, {
+    mode: 'development',
+    devtool: 'source-map',
+    output: {
+      filename: 'index.js',
+      path: devdir
+    },
+    devServer: {
+      port,
+      publicPath,
+      contentBase: devdir,
+      watchContentBase: true,
+      open: true,
+      hot: true,
+      overlay: true
+    },
+    plugins: [
+      new webpack.DllReferencePlugin({ manifest }),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  }),
+
+  pro: merge(baseConfig, {
+    mode: 'production',
+    output: {
+      filename: 'index.js',
+      path: resolve(__dirname, 'dist/webpack/pro')
+    },
+    plugins: [
+      new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } })
+    ]
+  }),
+
+  pre: merge(baseConfig, {
+    mode: 'production',
+    output: {
+      filename: 'index.js',
+      path: resolve(__dirname, 'dist/webpack/prepack')
+    },
+    plugins: [
+      new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+      new PrepackWebpackPlugin()
+    ]
+  })
+}
+
+const { npm_lifecycle_event } = process.env
+const suffix = npm_lifecycle_event.split(':')[1]
+export default configs[suffix]
